f5c0274701347a48426452f320243389198e93ab
FreeZZZeee <amazing.leo@yandex.ru>
2023-09-11 10:34:29 +0500

    neumorphism-login-page

---
+++ script.js
@@ -0,0 +1,39 @@
+let switchFrm = document.querySelector('#switch-frm');
+let switchF1 = document.querySelector('#switch-f1');
+let switchF2 = document.querySelector('#switch-f2');
+let switchCircle = document.querySelectorAll('.switch-circle');
+let switchBtn = document.querySelectorAll('.switch-btn');
+let regFrm = document.querySelector('#register-form');
+let loginFrm = document.querySelector('#login-form');
+let allButtons = document.querySelectorAll('.submit');
+
+let getButtons = (e) => e.preventDefault();
+
+let changeForm = (e) => {
+    switchFrm.classList.add('is-gx');
+    setTimeout(() => {
+        switchFrm.classList.remove('is-gx');
+    }, 1500);
+
+    switchFrm.classList.toggle('is-txr');
+    switchCircle[0].classList.toggle('is-txr');
+    switchCircle[1].classList.toggle('is-txr');
+
+    switchF1.classList.toggle('is-hidden');
+    switchF2.classList.toggle('is-hidden');
+    regFrm.classList.toggle('is-txl');
+    loginFrm.classList.toggle('is-txl');
+    loginFrm.classList.toggle('is-z200');
+};
+
+let mainF = (e) => {
+    for (let i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener('click', getButtons);
+    }
+
+    for (let i = 0; i < switchBtn.length; i++) {
+        switchBtn[i].addEventListener('click', changeForm);
+    }
+};
+
+window.addEventListener('load', mainF);
